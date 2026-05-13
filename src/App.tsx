5ed512950bbf8b7c702c1997da6982f3f7a29832
YamiYamata <58050514+Yami02@users.noreply.github.com>
2026-05-13 15:45:09 -0300

    teste-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import CodexModule from './components/CodexModule';
 import { Naturalista } from './pages/Naturalista';
